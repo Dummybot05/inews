552265416e1f3faf8560a1a22728c9dcab704d5d
Dummybot <dummybot0505@gmail.com>
2023-08-07 19:20:35 +0530

    first deploy

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const NewsAPI = require('newsapi')
+const path = require("path")
+const cors = require("cors")
+require('dotenv').config();
+const PORT = process.env.PORT || 3000
+const newsapi = new NewsAPI(process.env.API_KEY)
+const app = express()
+
+const countries = ["Argentina", "Australia", "Austria", "Belgium", "Brazil", "Bulgaria", "Canada", "China", "Colombia", "Cuba", "Czech Republic", "Egypt", "France", "Germany", "Greece", "Hong Kong", "Hungary", "India", "Indonesia", "Ireland", "Israel", "Italy", "Japan", "Latvia", "Lithuania", "Malaysia", "Mexico", "Morocco", "Netherlands", "New Zealand", "Nigeria", "Norway", "Philippines", "Poland", "Portugal", "Romania", "Russia", "Saudi Arabia", "Serbia", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Sweden", "Switzerland", "Taiwan", "Thailand", "Turkey", "UAE", "Ukraine", "United Kingdom", "United States", "Venezuela"];
+const countryCode = ["ar", "au", "at", "be", "br", "bg", "ca", "cn", "co", "cu", "cz", "eg", "fr", "de", "gr", "hk", "hu", "in", "id", "ie", "il", "it", "jp", "lv", "lt", "my", "mx", "ma", "nl", "nz", "ng", "no", "ph", "pl", "pt", "ro", "ru", "sa", "rs", "sg", "sk", "si", "za", "kr", "se", "ch", "tw", "th", "tr", "ae", "ua", "gb", "us", "ve"];
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
+
+app.get("/", (req, res) => {
+   res.render("home")
+})
+
+var a1 = undefined
+app.get("/mainpage", async(req, res) => {
+   countryD = req.query.id
+   await newsapi.v2.topHeadlines({ country: countryD }).then((res) => {
+       a1 = res.articles;
+   })
+   res.render("mainpage", {
+        countryNameIs: countryD,
+        respond1: a1,
+        lister: countries,
+        codes: countryCode
+   })
+})
+
+app.listen(PORT)
